469355476b0eaec84425855a53543a3ad199bb7b
bonjin <bonjin.app@gmail.com>
2023-08-07 18:15:16 +0900

    feat: camera preview 미리보기 기능 추가

--- app/src/main/java/kr/co/bonjin/activity/CameraActivity.kt
+++ app/src/main/java/kr/co/bonjin/activity/CameraActivity.kt
@@ -1,15 +1,27 @@
 package kr.co.bonjin.activity
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
-import android.view.SurfaceHolder
+import android.os.Handler
+import android.os.HandlerThread
+import android.view.Surface
+import android.view.TextureView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kr.co.bonjin.databinding.ActivityCameraBinding
 
-class CameraActivity: AppCompatActivity(), SurfaceHolder.Callback {
+class CameraActivity: AppCompatActivity()  {
     lateinit var binding: ActivityCameraBinding
+    lateinit var cameraManager: CameraManager
+    lateinit var handler: Handler
+    lateinit var cameraDevice: CameraDevice
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +32,46 @@ class CameraActivity: AppCompatActivity(), SurfaceHolder.Callback {
     }
 
     private fun init() {
+        performPermission()
 
         binding.closeButton.setOnClickListener {
             finish()
         }
 
+        val handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+        binding.textureView.surfaceTextureListener = object: TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+                openCamera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(
+                surface: SurfaceTexture,
+                width: Int,
+                height: Int
+            ) {
+            }
+
+            override fun onSurfaceTextureDestroyed(surface: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(surface: SurfaceTexture) {
+            }
+
+        }
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE)as CameraManager
+    }
+
+    private fun performPermission() {
         val cameraCheck = ContextCompat.checkSelfPermission(this@CameraActivity, Manifest.permission.CAMERA)
-        if(cameraCheck != PackageManager.PERMISSION_GRANTED) {
+        if(cameraCheck == PackageManager.PERMISSION_DENIED) {
             requestPermissions(
                 arrayOf(Manifest.permission.CAMERA), 1
             )
@@ -34,7 +79,46 @@ class CameraActivity: AppCompatActivity(), SurfaceHolder.Callback {
     }
 
     private fun openCamera() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        cameraManager.openCamera(cameraManager.cameraIdList.first(), object: CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
 
+                var surfaceTexture = binding.textureView.surfaceTexture
+                var surface = Surface(surfaceTexture)
+
+                var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequest.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        session.setRepeatingRequest(captureRequest.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                    }
+
+                },  handler)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+            }
+
+        }, handler)
     }
 
     override fun onRequestPermissionsResult(
@@ -46,18 +130,8 @@ class CameraActivity: AppCompatActivity(), SurfaceHolder.Callback {
 
         if(requestCode == 1) {
 
+        } else {
+            finish()
         }
-    }
-
-    override fun surfaceCreated(holder: SurfaceHolder) {
-        TODO("Not yet implemented")
-    }
-
-    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun surfaceDestroyed(holder: SurfaceHolder) {
-        TODO("Not yet implemented")
     }
 }
